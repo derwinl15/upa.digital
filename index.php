7bad8daca853bf8d0c569774851166d92ac72b86
Rigobero Meneses <meneses.rigoberto@gmail.com>
2021-04-20 12:32:37 -0300

    Subir comentario rigo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 define ("EXP",6000000);
 setlocale (LC_CTYPE, 'es_ES');
 ini_set ("display_errors","0");
-ini_set ("memory_limit","-1");
+ini_set ("memory_limit","-1"); //
 
 include_once 'lib/funciones.php';
 include_once 'lib/mysqlclass.php';
